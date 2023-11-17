a034ac556aa49c4b49d2246e941bbb5403fa2c63
ViniRomano <viniciusromanovpr@gmail.com>
2023-11-16 23:55:43 -0300

    first commit

---
+++ src/main/java/br/com/projeto/estoque_automotivo/EstoqueAutomotivoApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto.estoque_automotivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueAutomotivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueAutomotivoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/projeto/estoque_automotivo/EstoqueAutomotivoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto.estoque_automotivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueAutomotivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
