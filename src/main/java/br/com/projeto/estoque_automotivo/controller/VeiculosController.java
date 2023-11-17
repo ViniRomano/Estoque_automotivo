a26c9c499bb907b7bec0b655210374c35492bc3c
SantosAlisson <alisson.martins@mail.usf.edu.br>
2023-11-17 17:04:23 -0300

    New commit

--- src/main/java/br/com/projeto/estoque_automotivo/controller/VeiculosController.java
+++ src/main/java/br/com/projeto/estoque_automotivo/controller/VeiculosController.java
@@ -3,6 +3,9 @@ package br.com.projeto.estoque_automotivo.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.projeto.estoque_automotivo.Veiculo;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,7 +14,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class VeiculosController {
 
     @GetMapping
-    public List<veiculo> listar() {
+    public List<Veiculo> listar() {
+        return null;
 
     }
     
